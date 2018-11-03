43f669bc1fd8ee4bfc7d139dfbd7227749ce4624
Michael Stapleton <michaelstapleton@Mikes-MacBook-Pro.local>
2018-11-02 18:48:08 -0600

    gif of the world

---
+++ app.js
@@ -0,0 +1,1 @@
+<img src="https://img1.picmix.com/output/stamp/normal/3/0/7/2/552703_a96f2.gif" alt="globe" class="gif">
---
+++ readme.md
@@ -0,0 +1,2 @@
+"you become what you were always meant to be, a really big fish"
+-Big Fish, Tim Burton
